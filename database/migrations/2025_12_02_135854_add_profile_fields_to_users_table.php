648bb84422fc9002d50abe3a4ee16a7cade02a03
Wiamxb <bolawiam@gmail.com>
2025-12-02 15:04:43 +0100

    Extra profielvelden toegevoegd aan users tabel

---
+++ database/migrations/2025_12_02_135854_add_profile_fields_to_users_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username')->nullable();
+            $table->date('birthday')->nullable();
+            $table->text('about_me')->nullable();
+            $table->string('profile_photo')->nullable();
+        });
+    }
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['username', 'birthday', 'about_me', 'profile_photo']);
+        });
+    }
+};

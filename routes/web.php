a429c7a457d87dc5f7c2052fcaf070285e2c5d63
Wiamxb <bolawiam@gmail.com>
2025-12-05 00:34:22 +0100

    Routes voor nieuwsitems toegevoegd

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\NewsItemController;
 
 Route::view('/', 'welcome');
 
@@ -19,7 +20,19 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/profile', [ProfileController::class, 'update'])->name('profile.update');
 });
 
+// Publiek nieuws
+Route::get('/news', [NewsItemController::class, 'index'])->name('news.index');
+Route::get('/news/{newsItem}', [NewsItemController::class, 'show'])->name('news.show');
+
+// Admin (ingelogd)
+Route::middleware(['auth'])->group(function () {
+    Route::get('/admin/news/create', [NewsItemController::class, 'create'])->name('news.create');
+    Route::post('/admin/news', [NewsItemController::class, 'store'])->name('news.store');
+    Route::get('/admin/news/{newsItem}/edit', [NewsItemController::class, 'edit'])->name('news.edit');
+    Route::put('/admin/news/{newsItem}', [NewsItemController::class, 'update'])->name('news.update');
+    Route::delete('/admin/news/{newsItem}', [NewsItemController::class, 'destroy'])->name('news.destroy');
+});
+
 // Auth routes van Breeze
 require __DIR__.'/auth.php';
-
 

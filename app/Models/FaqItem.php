d738defb07918c26a7ee1cf450332ffb173bbae9
Wiamxb <bolawiam@gmail.com>
2025-12-11 22:37:41 +0100

    Models voor FAQ categorieën en items toegevoegd

---
+++ app/Models/FaqItem.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FaqItem extends Model
+{
+    protected $fillable = [
+        'question',
+        'answer',
+        'faq_category_id'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(FaqCategory::class);
+    }
+}

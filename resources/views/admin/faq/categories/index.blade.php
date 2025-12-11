0d6bae2e05b69f6a298433e373707f2dc19e26cd
Wiamxb <bolawiam@gmail.com>
2025-12-11 23:14:02 +0100

    Admin FAQ categorieën overzicht view aangemaakt

---
+++ resources/views/admin/faq/categories/index.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            FAQ categorieën (admin)
+        </h2>
+    </x-slot>
+
+    <div class="p-6">
+        <p>Admin FAQ categorie overzicht werkt.</p>
+    </div>
+</x-app-layout>

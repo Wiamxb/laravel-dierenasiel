c7198cc919504204905084ecc4fc4a9d017e5e22
Wiamxb <bolawiam@gmail.com>
2025-12-23 16:48:34 +0100

    Contact controller en model toegevoegd

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ContactMessage;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function create()
+    {
+        return view('contact');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'message' => 'required|string',
+        ]);
+
+        ContactMessage::create($request->only('name', 'email', 'message'));
+
+        return redirect()->back()->with('success', 'Bericht verzonden!');
+    }
+}

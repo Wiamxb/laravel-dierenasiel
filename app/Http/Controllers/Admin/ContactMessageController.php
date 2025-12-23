dea241fcb0b36e06b8a52b38829fb4a976c3925f
Wiamxb <bolawiam@gmail.com>
2025-12-23 17:07:01 +0100

    Admin controller voor contactberichten toegevoegd

---
+++ app/Http/Controllers/Admin/ContactMessageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\ContactMessage;
+
+class ContactMessageController extends Controller
+{
+    public function index()
+    {
+        $messages = ContactMessage::latest()->get();
+
+        return view('admin.contact.index', compact('messages'));
+    }
+}

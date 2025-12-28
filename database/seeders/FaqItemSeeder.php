83e79c760675bae2308b829d4ecfedc1de71c03f
Wiamxb <bolawiam@gmail.com>
2025-12-28 19:18:14 +0100

    FAQ-seeders uitgebreid met meerdere vragen per categorie

--- database/seeders/FaqItemSeeder.php
+++ database/seeders/FaqItemSeeder.php
@@ -12,7 +12,10 @@ class FaqItemSeeder extends Seeder
     {
         $algemeen = FaqCategory::where('name', 'Algemeen')->first();
         $adoptie = FaqCategory::where('name', 'Adoptie')->first();
+        $vrijwilligers = FaqCategory::where('name', 'Vrijwilligers')->first();
+        $donaties = FaqCategory::where('name', 'Donaties')->first();
 
+        /*ALGEMEEN*/
         FaqItem::create([
             'question' => 'Wat zijn de openingsuren van het dierenasiel?',
             'answer' => 'Wij zijn elke dag geopend van 10u tot 17u.',
@@ -20,9 +23,48 @@ class FaqItemSeeder extends Seeder
         ]);
 
         FaqItem::create([
+            'question' => 'Waar is het dierenasiel gelegen?',
+            'answer' => 'Ons dierenasiel bevindt zich in Brussel en is vlot bereikbaar met het openbaar vervoer.',
+            'faq_category_id' => $algemeen->id,
+        ]);
+
+        /*ADOPTIE*/
+        FaqItem::create([
             'question' => 'Hoe kan ik een dier adopteren?',
             'answer' => 'Adopties verlopen via een kennismakingsgesprek in het asiel.',
             'faq_category_id' => $adoptie->id,
+        ]);
+
+        FaqItem::create([
+            'question' => 'Zijn alle dieren gevaccineerd?',
+            'answer' => 'Ja, alle dieren worden gecontroleerd en gevaccineerd voor adoptie.',
+            'faq_category_id' => $adoptie->id,
+        ]);
+
+        /*VRIJWILLIGERs*/
+        FaqItem::create([
+            'question' => 'Hoe kan ik vrijwilliger worden?',
+            'answer' => 'Je kan ons contacteren via het contactformulier om vrijwilliger te worden.',
+            'faq_category_id' => $vrijwilligers->id,
+        ]);
+
+        FaqItem::create([
+            'question' => 'Moet ik ervaring hebben met dieren?',
+            'answer' => 'Nee, motivatie is belangrijker dan ervaring. Begeleiding wordt voorzien.',
+            'faq_category_id' => $vrijwilligers->id,
+        ]);
+
+        /*DONATIES*/
+        FaqItem::create([
+            'question' => 'Hoe kan ik het dierenasiel steunen?',
+            'answer' => 'Je kan ons steunen via financiële donaties of door materiaal te schenken.',
+            'faq_category_id' => $donaties->id,
+        ]);
+
+        FaqItem::create([
+            'question' => 'Waarvoor worden donaties gebruikt?',
+            'answer' => 'Donaties worden gebruikt voor voeding, medische zorgen en onderhoud van het asiel.',
+            'faq_category_id' => $donaties->id,
         ]);
     }
 }
